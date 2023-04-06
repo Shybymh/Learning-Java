dccb4315dd28a12dc49febaa0c9fe88782b5f510
Shybymh <shybyhamza@gmail.com>
2023-04-05 22:06:38 -0500

    switch-expression

--- src/Grade.java
+++ src/Grade.java
@@ -9,10 +9,10 @@ public class Grade {
         Scanner scanner= new Scanner(System.in);
 
         int score = scanner.nextInt();
-        scanner.close();
+
 
         if ( score < 60) {
-            System.out.println("Your grade is E");
+            System.out.println("Your grade is F");
         } else if ( score < 70) {
             System.out.println("Your grade is D");
         } else if (score < 80) {
@@ -21,6 +21,21 @@ public class Grade {
             System.out.println("Your grade is B");
         } else {
             System.out.println("You grade is A");  }
+
+        System.out.println("Enter your grade");
+        String grade = scanner.next();
+        scanner.close();
+
+        String message;
+        message = switch(grade){
+            case "A" -> "Excellent Job!!";
+            case "B" -> "Great Job";
+            case "C" -> "Good Job";
+            case "D" -> "Work Harder";
+            case "F" -> "Meet the Teacher";
+            default -> "Error";
+        };
+        System.out.println(message);
     }
 
 }
