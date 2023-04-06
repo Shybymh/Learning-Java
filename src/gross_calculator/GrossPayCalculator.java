e27a434c4f39d3e25e6573be776d3903276aef35
Shybymh <shybyhamza@gmail.com>
2023-04-05 23:00:55 -0500

    While-loop

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -5,23 +5,26 @@ import java.util.Scanner;
 public class GrossPayCalculator {
 
     public static void main(String[] args){
-        int hours = 0;
+        int payRate = 15;
+        int maxHours =40;
+        double hoursWorked = 0;
         System.out.println("How many hours did you work?");
 
         Scanner scanner = new Scanner(System.in);
-        hours = scanner.nextInt();
+        hoursWorked = scanner.nextInt();
 
-        double payRate;
-        System.out.println("What is your pay rate?");
-        payRate = scanner.nextDouble();
 
+        while (hoursWorked>maxHours) {
+            System.out.println("Invalid. Hoursa must be between 1 and 40.");
+            hoursWorked = scanner.nextDouble();
+        }
 
 
         scanner.close();
 
-        double grossPay = hours * payRate;
+        double grossPay = hoursWorked * payRate;
 
-        System.out.println("Gross pay: " + grossPay);
+        System.out.println("Gross pay: $" + grossPay);
 
 
     }
